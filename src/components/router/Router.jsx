5063557ea613cb94c3938d5be13ee6668b22e4e7
AngeloDiMortimer <70825802+AngeloDiMortimer@users.noreply.github.com>
2023-07-25 16:17:00 -0400

    Created Routes and Home component for testing

--- src/components/router/Router.jsx
+++ src/components/router/Router.jsx
@@ -1,1 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "../App";
+//import Home from "../../pages/Home";
+
+const Router = () => {
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <App />
+        },
+    ]);
+
+    return <RouterProvider router={router} />;
+}
+
+export default Router;
