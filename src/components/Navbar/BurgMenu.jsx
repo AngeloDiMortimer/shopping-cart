b6e98facca4f75637b458fb7e0cce3eb0fa2ae68
AngeloDiMortimer <70825802+AngeloDiMortimer@users.noreply.github.com>
2023-07-26 17:49:15 -0400

    finished Burguer Menu for mobile usage

---
+++ src/components/Navbar/BurgMenu.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const BurgMenu = () => {
+    const [toggle, setToggle] = useState(false);
+    const [isHidden, setIsHidden] = useState("");
+
+    const handleOpen = () => {
+        setToggle(!toggle);
+        if (!isHidden) {
+            setIsHidden("open");
+            } else setIsHidden("");
+    }
+
+    return (
+        <>
+            <button id="menu-btn" className={`block hamburger md:hidden focus:outline-none mr-4 ${isHidden}`} 
+            onClick={handleOpen}>
+              <span className="hamburger-top"></span>
+              <span className="hamburger-middle"></span>
+              <span className="hamburger-bottom"></span>
+            </button>
+
+            { toggle &&
+                <div className="md:hidden">
+                    <div id="menu" className={`absolute flex flex-col items-center self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md`}>
+                        <a href="#" className="">Categories</a>
+                        <a href="#" className="">Products</a>
+                        <a href="#" className="">About Us</a>
+                    </div>
+                </div> 
+            } 
+           
+        </>
+    )
+}
+
+export default BurgMenu;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,34 @@
+import { IconContext } from "react-icons";
 import { FaCartShopping } from "react-icons/fa6";
+import BurgMenu from "./BurgMenu";
 
 const Navbar = () => {
+    
 
     return (
-        <div className="relative container mx-auto p-6">
+        <div className="relative  mx-auto p-6">
             <div className="flex items-center justify-between">
                 <div className="logo">
                     <h1>Placeholder</h1>
                 </div>
 
-                <div className="hidden md:flex space-x-6">
-                <a href="#" className="">Categories</a>
-                <a href="#" className="">Products</a>
-                <a href="#" className="">About Us</a>
+                <div className="flex items-center">
+                    <div className="hidden md:flex space-x-6 mr-8">
+                        <a href="#" className="">Categories</a>
+                        <a href="#" className="">Products</a>
+                        <a href="#" className="">About Us</a>
+                    </div>
+
+                    {/* Mobile Menu*/}
+                    <BurgMenu />
+
+                    {/* Cart Icon */}
+                    <IconContext.Provider value={{color: "blue", size: 26, className: "color-green-600"}}> 
+                        <div>
+                            <FaCartShopping />
+                        </div>
+                    </IconContext.Provider>
                 </div>
-                <FaCartShopping />
             </div>
         </div>
     )
