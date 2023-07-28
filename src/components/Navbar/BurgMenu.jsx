e1fae3bf70910c427ed2f63b62f38d1ef4614418
AngeloDiMortimer <70825802+AngeloDiMortimer@users.noreply.github.com>
2023-07-28 16:53:45 -0400

    Added onClick events to BurgMenu

--- src/components/Navbar/BurgMenu.jsx
+++ src/components/Navbar/BurgMenu.jsx
@@ -24,9 +24,19 @@ const BurgMenu = () => {
             { toggle &&
                 <div className="md:hidden">
                     <div id="menu" className={`absolute z-40 font-semibold bg-slate-950 flex flex-col items-center w-full self-end py-8 mt-10 space-y-6 bg-white sm:w-auto sm:self-center right-0 drop-shadow-md`}>
-                        <Link to="home" className="">Home</ Link>
-                        <Link to="products" className="">Products</ Link>
-                        <Link to="about" className="">About Us</ Link>
+                        
+                        <div onClick={handleOpen}>
+                            <Link to="home" className="">Home</ Link>
+                        </div>
+                        
+                        <div onClick={handleOpen}>
+                            <Link to="products" className="">Products</ Link>
+                        </div>
+
+                        <div onClick={handleOpen}>
+                            <Link to="about" className="">About Us</ Link>
+                        </div>
+                        
                     </div>
                 </div> 
             } 
