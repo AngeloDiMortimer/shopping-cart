999b98183da44bd832aa6872176627047597a570
AngeloDiMortimer <70825802+AngeloDiMortimer@users.noreply.github.com>
2023-07-31 18:21:07 -0400

    Added styling

--- src/components/ProudProducts.jsx
+++ src/components/ProudProducts.jsx
@@ -5,9 +5,9 @@ const ProudProducts = () => {
 
 
     return (
-        <div className="proud-container">
-            <h2>Products we are proud of</h2>
+        <div className="proud-container p-4 flex flex-col items-center"> 
             <div className="container">
+                <h2 className="text-3xl font-semibold">Products we are proud of</h2>
                 <div className="products-grid">
                     <ProductItem />
                 </div>
